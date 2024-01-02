a199eac52cb746f194877a6c80623e97d8df91c7
Kangkhita Urbee <86801356+KangkhitaUrbee@users.noreply.github.com>
2024-01-02 18:33:04 +0600

    Delete Retail-Store-Frontend directory

--- Retail-Store-Frontend/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// Create an Axios instance with a custom configuration
-const instance = axios.create({
-  baseURL: 'http://localhost:3000', // Replace with your actual backend API base URL
-  withCredentials: true, // Include credentials (e.g., cookies) with requests
-});
-
-export default instance;
