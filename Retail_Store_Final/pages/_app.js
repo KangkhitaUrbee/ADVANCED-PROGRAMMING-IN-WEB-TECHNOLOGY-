6451cff41ed7df20a14d3bc1611118f0531b5922
Nuzhat Tanzina Prova <115353245+NuzhatTanzina@users.noreply.github.com>
2024-01-03 06:21:29 +0600

    Add files via upload

--- Retail_Store_Final/pages/_app.js
+++ Retail_Store_Final/pages/_app.js
@@ -1,12 +1,18 @@
-import React from "react";
-import "../styles/globals.css";
-import { AuthProvider } from "./session/authcontext";
+import "@/styles/globals.css";
+
+import 'daisyui/dist/full.css'; // Import DaisyUI styles
+
+
+
+import { AuthProvider } from "./utils/authcontext";
 
 export default function App({ Component, pageProps }) {
   return (
+
+    
     <AuthProvider>
+      <script src="https://cdn.tailwindcss.com"></script>
       <Component {...pageProps} />
     </AuthProvider>
   );
 }
-
